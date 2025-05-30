9493425bd0995fd032d6518f163865280a8488c0
방진혁 <jinhyeokfang@gmail.com>
2025-05-30 09:15:36 +0900

    chore: redisson 추가

---
+++ src/main/kotlin/uk/jinhy/libraryreservationserver/global/config/RedissonClientConfig.kt
@@ -0,0 +1,25 @@
+package uk.jinhy.libraryreservationserver.global.config
+
+import org.redisson.Redisson
+import org.redisson.api.RedissonClient
+import org.redisson.config.Config
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class RedissonClientConfig {
+
+    @Value("\${spring.data.redis.host}")
+    private lateinit var host: String
+
+    @Value("\${spring.data.redis.port}")
+    private lateinit var port: String
+
+    @Bean
+    fun redissonClient(): RedissonClient {
+        val config = Config()
+        config.useSingleServer().setAddress("redis://$host:$port")
+        return Redisson.create(config)
+    }
+}
